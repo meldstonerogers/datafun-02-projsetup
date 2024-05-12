51af37bd203c651906a3192ae32875b32cf32a15
Melissa Stone Rogers <168462484+meldstonerogers@users.noreply.github.com>
2024-05-12 14:37:45 -0500

    Module 2 Project- creating folders

--- melissastonerogers_projsetup.py
+++ melissastonerogers_projsetup.py
@@ -1,21 +1,61 @@
-"""starting module 2 project"""
+"""Module 2 Creating Project Folders"""
 
+#import dependencies 
 import pathlib
+import time
+import MelissaStoneRogers_utils
 
-
-def create_project_directory(directory_name: str): -> None:    
+#create folder for range
+def create_year_folders(start_year, end_year):
     """
-    Creates a project sub-directory.
-    :param directory_name: Name of the directory to be created, e.g. "test"
+    Creates folders for a given range of years.
+    :param start_year: First year, e.g. 2010
+    :param end_year: Final year, e.g. 2024
     """
-    pathlib.Path(directory_name).mkdir(exist_ok=True)
+    for year in range(start_year, end_year + 1):
+        folder_name = str(year)
+        pathlib.Path(folder_name).mkdir(exist_ok=True)
 
+#create folder from list
+def create_folders_from_list(folder_list):
+    for folder in folder_list:
+        pathlib.Path(folder).mkdir(exist_ok=True)
 
+#create prefixed folders
+def create_prefixed_folders(folder_list, prefix):
+    for folder in folder_list:
+        pathlib.Path(f"{prefix}-{folder}").mkdir(exist_ok=True)
+
+#create folders periodically 
+def create_folders_periodically(duration, period):
+    start_time = time.time()
+    while time.time() - start_time < duration: 
+        current_time = time.time() - start_time
+        pathlib.Path(f"Folder created at {round(current_time)} seconds").mkdir(exist_ok=True)
+        time.sleep(period)
+
+#define main function 
 def main():
-    """Scaffold a project"""
-    create_project_directory(directory_name='test')  #name the parameter
-    create_project_directory(directory_name='docs')  #name the parameter
+    ''' Main function to demonstrate module capabilities. '''
+    #print byline from imported module
+    print(f"Byline: {MelissaStoneRogers_utils}")
 
+    # Call function 1 to create folders for a range (e.g. years)
+    create_year_folders(start_year=2010, end_year=2024)
 
-    if __name__ == '__main__':
-        main()
+    # Call function 2 to create folders given a list
+    folder_names = ['month-week1', 'month-week2', 'month-week3']
+    create_folders_from_list(folder_names)
+
+    # Call function 3 to create folders using comprehension
+    folder_names = ['week1', 'week2', 'week3']
+    prefix = 'month'
+    create_prefixed_folders(folder_names, prefix)
+
+    # Call function 4 to create folders periodically using while
+    duration_secs = 5  
+    create_folders_periodically(duration_secs, 1)
+
+   
+if __name__ == '__main__':
+    main()
